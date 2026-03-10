44beeb5f6c0a11894f9258e5f2b8dc5aa2443271
Nigel Omondi <nigelomondi62@gmail.com>
2026-03-10 12:39:11 +0300

    Express Server

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Shopify Odoo Integration!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+export default app;
