6145e60380f66648c4b560df6f5b88c2b9ba1656
Nigel Omondi <nigelomondi62@gmail.com>
2026-03-10 12:53:35 +0300

    product update from odoo

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ app.get('/', (req, res) => {
     res.send('Shopify Odoo Integration!');
 });
 
+app.post('/webhook/odoo-product-update', (req, res) => {
+    console.log('Received webhook data from Odoo:', req.body);
+    // Process the webhook data here (e.g., sync to Shopify)
+    res.status(200).send('Webhook received');
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
